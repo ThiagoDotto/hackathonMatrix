01a67c0aec07ed5c0c65b4b01a240ad80ef248eb
Caio Victor <caiovictormota@gmail.com>
2018-11-10 16:12:11 -0200

    Classe Agente

---
+++ src/main/java/hacktahon/config/model/Agente.java
@@ -0,0 +1,27 @@
+package hacktahon.config.model;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Agente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Ponto ponto;
+	
+	public Agente(Ponto p) {
+		this.ponto = p;
+	}
+
+	public Ponto getPonto() {
+		return ponto;
+	}
+
+	public void setPonto(Ponto ponto) {
+		this.ponto = ponto;
+	}
+}

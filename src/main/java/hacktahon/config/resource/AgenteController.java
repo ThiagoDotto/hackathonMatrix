3fe088e6e0848c4e66403b5e9a963aa73439d418
Caio Victor <caiovictormota@gmail.com>
2018-11-10 16:32:05 -0200

    AgenteController

---
+++ src/main/java/hacktahon/config/resource/AgenteController.java
@@ -0,0 +1,31 @@
+package hacktahon.config.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hacktahon.config.model.Agente;
+import hacktahon.config.model.Ponto;
+import hacktahon.config.repository.AgenteRepository;
+
+@RestController
+public class AgenteController {
+	
+	@Autowired
+	private AgenteRepository agentes;
+	
+	@GetMapping("/map")
+	public String enemySpawn() {
+		
+		Ponto ponto = new Ponto();
+		ponto.setCoordX("-220.121");
+		ponto.setCoordY("220.111");
+		ponto.setSeguro(true);
+		
+		Agente enemy = new Agente(ponto);
+		agentes.save(enemy);
+		
+		return "teste";
+	}
+
+}
